23b3e519fb4b6a699f5ac7643f4b48c8f2281e60
burgere <eric.burger@comcast.net>
2018-01-28 19:58:10 -0600

    Move container component

--- webpack.client.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require(webpack);
-
-module.exports = {
-    plugins: [
-        new webpack.DefinePlugin({
-            PIVOTAL_TRACKER_API_TOKEN: JSON.stringify(process.env.PIVOTAL_TRACKER_API_TOKEN)
-        })
-    ]
-}
